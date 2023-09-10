0b82743f9cc81eb66b64e448c2a8644ef9a474ed
alphin <alphinpoly8@gmail.com>
2023-09-10 19:21:39 +0530

    added search button and some other css and html

--- admin/java.js
+++ admin/java.js
@@ -1,3 +1,6 @@
 document.getElementById("menu-button").addEventListener("click", function () {
     document.getElementById("sidebar").classList.toggle("active");
 });
+document.getElementById("search-button").addEventListener("click", function (){
+    document.getElementById("search").classList.toggle("add");
+});
