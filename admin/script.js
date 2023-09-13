361026d75fb13729f2f4d2d0695b80ed61e41fdf
alphin <alphinpoly8@gmail.com>
2023-09-13 23:19:41 +0530

    added some responsive layout and removed search bar and updated the css

--- admin/script.js
+++ admin/script.js
@@ -1,6 +1,3 @@
 document.getElementById("menu-button").addEventListener("click", function () {
     document.getElementById("sidebar").classList.toggle("active");
 });
-document.getElementById("search-button").addEventListener("click", function (){
-    document.getElementById("search").classList.toggle("add");
-});
