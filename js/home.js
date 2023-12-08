11de8e267f45666cd42a1f3fcc6c791c0f1bfb50
alphin <alphinpoly8@gmail.com>
2023-12-08 15:58:33 +0530

    added search bar in home page

--- js/home.js
+++ js/home.js
@@ -1,3 +1,10 @@
 document.getElementById("menu-icon").addEventListener("click", function() {
     document.getElementById("option-menu-list").classList.toggle("active");
 });
+
+document.getElementById("filter").addEventListener("click", function () {
+  document.getElementById("filter-search").classList.toggle("show-filters");
+});
+document.getElementById("filter").addEventListener("click", function () {
+  document.getElementById("filter-form").classList.toggle("show-filters");
+});
